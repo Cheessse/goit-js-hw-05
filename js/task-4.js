7cff0852303acbabc709da31bc463dc354cc7d17
David <143638937+Cheessse@users.noreply.github.com>
2023-12-18 12:03:00 +0000

    fixed mistakes

--- js/task-4.js
+++ js/task-4.js
@@ -5,7 +5,7 @@ const getTotalBalanceByGender = (users, gender) => {
   return genderBalance;
 };
 
-const allUsers = [
+const allUserss = [
   {
     name: "Moore Hensley",
     gender: "male",
@@ -43,6 +43,6 @@ const allUsers = [
   },
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUserss, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUserss, "female")); // 8863
